d30a0b505d532669e0507f870fb26c32871c76ae
AnviRishi <AnviRishi@AnviRishi-PC>
2016-12-13 17:15:36 +0530

    tetst commit

---
+++ helloworld/src/helloworld/TEst.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class TEst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hai...");
+
+//prashant adding
+	}
+
+}
